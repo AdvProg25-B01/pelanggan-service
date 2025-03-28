e274bd1e183c9456221fb179f5cb219aa346bb90
sopigoo <shofinadhifa24@gmail.com>
2025-03-28 20:30:28 +0700

    Initial commit

---
+++ src/test/java/id/ac/ui/cs/advprog/pelangganservice/PelangganServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.pelangganservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PelangganServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
