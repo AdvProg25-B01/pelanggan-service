dbad92f92b14dfa26bdf4bb0a75d04148c5b853a
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-04-11 11:03:35 +0700

    [REFACTOR] Add delete(String) method to CustomerRepository

--- src/main/java/id/ac/ui/cs/advprog/pelangganservice/repository/CustomerRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/pelangganservice/repository/CustomerRepository.java
@@ -5,4 +5,5 @@ import id.ac.ui.cs.advprog.pelangganservice.model.Customer;
 public interface CustomerRepository {
     void save(Customer customer);
     Customer findById(String id);
+    void delete(String id);  // Pastikan metode ini sudah ada!
 }
