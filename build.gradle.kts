5c0080c0c4795b4f6368c5ed2ed3fb33d5ffc691
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-05-08 17:27:16 +0700

    [BUILD] add h2database and starter data jpa

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Untuk JPA
+    runtimeOnly("com.h2database:h2") // Untuk database in-memory (baik untuk dev & test)
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
