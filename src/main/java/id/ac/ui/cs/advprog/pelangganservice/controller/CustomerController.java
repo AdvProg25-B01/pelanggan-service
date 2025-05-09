910ec2c74e61cf3821198b1e2bd070e9ae8c3d5f
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-05-09 13:47:15 +0700

    [RED] Implement Customer Controller

---
+++ src/main/java/id/ac/ui/cs/advprog/pelangganservice/controller/CustomerController.java
@@ -0,0 +1,19 @@
+package id.ac.ui.cs.advprog.pelangganservice.controller;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+
+// @Controller
+// @RequestMapping("/customers")
+public class CustomerController {
+
+    // Inject CustomerService
+    // Buat method untuk handle request (GET, POST, etc.)
+    // Contoh:
+    // @GetMapping
+    // public String listCustomers(Model model) {
+    //     model.addAttribute("customers", customerService.getAllCustomers());
+    //     return "customers-list"; // nama template Thymeleaf
+    // }
+}
