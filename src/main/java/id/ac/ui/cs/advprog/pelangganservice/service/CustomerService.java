4f0c5b6aefc2af5f5dca43a5f30d8091166ce0f3
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-05-22 02:37:52 +0700

    [REFACTOR] Implement CustomerService to use DTO

--- src/main/java/id/ac/ui/cs/advprog/pelangganservice/service/CustomerService.java
+++ src/main/java/id/ac/ui/cs/advprog/pelangganservice/service/CustomerService.java
@@ -2,16 +2,17 @@ package id.ac.ui.cs.advprog.pelangganservice.service;
 
 import id.ac.ui.cs.advprog.pelangganservice.model.Customer;
 import id.ac.ui.cs.advprog.pelangganservice.strategy.SearchStrategy;
+import id.ac.ui.cs.advprog.pelangganservice.dto.UpdateCustomerRequestDTO;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerService {
-    Customer createCustomer(Customer customer);
+    Customer createCustomer(Customer customer); // Bisa juga menerima CreateCustomerRequestDTO
     List<Customer> getAllCustomers();
     Optional<Customer> getCustomerById(UUID id);
-    Optional<Customer> updateCustomer(UUID id, Customer customerDetails);
+    Optional<Customer> updateCustomer(UUID id, UpdateCustomerRequestDTO customerDetailsDTO);
     boolean deleteCustomer(UUID id);
     List<Customer> searchCustomers(String searchTerm, SearchStrategy searchStrategy);
 }
